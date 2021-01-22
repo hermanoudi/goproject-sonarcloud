4a6cf63cbae8ebc5f07c068157a0559abddd1733
Hermano Flávio de Moura <hermanoudi@gmail.com>
2021-01-22 19:11:01 -0300

    fix: :ambulance: fix coverage code

--- sum.go
+++ sum.go
@@ -10,10 +10,10 @@ func sum(a int, b int) int {
 	return a + b
 }
 
-func times(a int, b int) int {
-	return a * b
-}
+//func times(a int, b int) int {
+//	return a * b
+//}
 
-func sub(a int, b int) int {
-	return a - b
-}
+//func sub(a int, b int) int {
+//	return a - b
+//}
